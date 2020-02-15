e69a27c12d4355b5297d963d10cd632dd0eee98c
gabrielnino99 <60182220+gabrielnino99@users.noreply.github.com>
2020-02-14 22:16:47 -0500

    Cambios

---
+++ Talleres/Taller1/ErrorRedondeo.R
@@ -0,0 +1,13 @@
+###############
+#Error de Redondeo
+#Autores:
+#Gabriel Niño
+#Juliana Garcia
+#Taller 1 - Analisis Numerico
+##############
+rm(list=ls())
+valreal<-536.78
+valaprox<-536.7
+E<-abs(valreal-valaprox)
+print(E)
+#-----------------------------------------------
